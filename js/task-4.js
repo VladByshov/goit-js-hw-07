34cb93d3f09804a0941c70dbe26f1ad0c29e1db5
Byshov <byshov1998@gmail.com>
2026-02-02 21:52:12 +0100

    done forth task

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,13 @@
+const form = document.querySelector('.login-form');
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+  const email = event.target.elements.email.value.trim();
+  const pass = event.target.elements.password.value.trim();
+  if(!email || !pass) {
+    alert("All form fields must be filled in");
+  }
+  console.log({email, pass});
+  form.reset();
+}
 
