97e8f625967a7287f32d979a04045db00f56b63d
Byshov <byshov1998@gmail.com>
2026-02-03 15:09:02 +0100

    done 5-th task

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,13 @@
+const changeColorBtn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
